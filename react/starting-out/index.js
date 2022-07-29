0208ee38d2e6dad007cd7bb4f5a301cd3191c6c1
Cam-Black <cameronablack1998@gmail.com>
2022-07-29 15:46:58 +0100

    Added a h1 element using React syntax. Commented out vanilla JS with comments explaining both parts.

--- react/starting-out/index.js
+++ react/starting-out/index.js
@@ -1,5 +1,15 @@
-const rootDiv = document.getElementById("root");
-const ele = document.createElement("h1");
+/**
+ * Plain JavaScript to render a header out to the webpage appending it to the DOM.
+ * Imperative way to program
+ */
+// const rootDiv = document.getElementById("root");
+// const ele = document.createElement("h1");
+//
+// ele.innerText = "Hello World";
+// rootDiv.appendChild(ele);
 
-ele.innerText = "Hello World";
-rootDiv.appendChild(ele);
+/**
+ * Same as above but using React instead.
+ * Declarative way to program.
+ */
+ReactDOM.render(<h1 className="header">Hello React!</h1>, document.getElementById("root"));
