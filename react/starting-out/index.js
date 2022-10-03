a56c450ae2a57175e5fc79b819792a252123c597
Cam-Black <cameronablack1998@gmail.com>
2022-10-03 16:51:53 +0100

    Commented out first rendered react element.

--- react/starting-out/index.js
+++ react/starting-out/index.js
@@ -1,3 +1,5 @@
-//Create a simple header on the page using createRoot instead of ReactDOM.render
+// Create a simple header on the page using createRoot instead of ReactDOM.render
 // as it is deprecated in React 18.
-ReactDOM.createRoot(root).render(<h1>Hello World</h1>);
+/** ReactDOM.createRoot(root).render(<h1>Hello World</h1>);
+ **/
+
