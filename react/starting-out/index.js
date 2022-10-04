0086cc19e9c34f6501031af8d1569a1048c5fd95
Cam-Black <cameronablack1998@gmail.com>
2022-10-04 10:48:56 +0100

    Fixed syntax issue to render the page variable content.

--- react/starting-out/index.js
+++ react/starting-out/index.js
@@ -1,4 +1,16 @@
-const root = document.getElementById("root");
-const header = document.createElement("h1");
-header.innerText = "Hello World";
-root.appendChild(header);
+// const h1 = document.createElement("h1")
+// h1.textContent = "Hello world"
+// h1.className = "header"
+// console.log(h1)
+
+// JSX
+const page = (
+    <div>
+        <h1 className="header">This is JSX</h1>
+        <p>This is a paragraph</p>
+    </div>
+)
+ReactDOM.render(
+    page,
+    document.getElementById("root")
+)
