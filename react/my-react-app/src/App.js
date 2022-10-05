831cd6357ea6d893201a5bb372b75a019fdbf8fd
Cam-Black <cameronablack1998@gmail.com>
2022-10-05 10:14:30 +0100

    Using axios and useState and useEffect, added a counter to the page that increments on click as its own component, generated a list of cat facts using a public API, and displayed each fact in its own p tag in a parent div.

--- react/my-react-app/src/App.js
+++ react/my-react-app/src/App.js
@@ -1,13 +1,48 @@
-import react from "react";
-import {useState} from "react";
+// import react, {useEffect} from "react";
+import {useState, useEffect} from "react";
+import axios from "axios";
+
+const getData = () => {
+    return axios.get("https://catfact.ninja/facts")
+        .then(res => {
+            console.log(res.data.data);
+            let i = 1;
+            const resultBlock = document.createElement("div");
+            document.querySelector("#root").appendChild(resultBlock);
+
+            for (let info of res.data.data) {
+                const fact = document.createElement("p");
+                fact.innerText = `Fact ${i}: ${info.fact}`;
+                resultBlock.appendChild(fact);
+                i++;
+            }
+            // return JSON.stringify(res.data, null, 2);
+        })
+        .catch(err => console.error(err));
+}
+
+function IncreaseCounter() {
+    const [counter, setCounter] = useState(0);
+
+    return (
+        <button onClick={() => setCounter(prevState => prevState + 1)}>Clicks: {counter}</button>
+    )
+}
 
 function App() {
-    const [counter, setCounter] = useState(0);
+    const [jsonString, setJsonString] = useState("");
+
+    useEffect(() => {
+        getData().then(data => {
+            setJsonString(() => data);
+        })
+    }, []);
 
     return (
         <div className="App">
             <h1>Hello CodeSandbox</h1>
-            <button onClick={() => setCounter(prevState => prevState + 1)}>Clicks: {counter}</button>
+            <IncreaseCounter/>
+            <p>{jsonString}</p>
         </div>
     );
 }
